6d30e5be1674758f8ea06b7d83abea01139ef5a3
mew22 <sebastien.delaherche@gmail.com>
2014-01-16 16:38:16 +0100

    Ajout des sous dossiers images + css + pages + script

---
+++ pages/index1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./style.css">
+        <title></title>
+    </head>
+    <body>
+        
+        <header>
+		<h1>BANNIERE</h1>
+	</header>
+        <?php
+            
+        
+        ?>
+        <section>
+		<article>
+		</article>
+
+		<aside>
+		</aside>
+	</section>
+
+	<footer>
+		<p></p>
+	</footer>
+    </body>
+</html>
